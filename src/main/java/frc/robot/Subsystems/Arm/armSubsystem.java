fd33bfa562ec2a2789165fb6571d7e1ecc777260
Driver Station <FRC_3467_Driver@team3467.org>
2024-02-04 17:09:55 -0500

    T

--- src/main/java/frc/robot/Subsystems/Arm/armSubsystem.java
+++ src/main/java/frc/robot/Subsystems/Arm/armSubsystem.java
@@ -25,8 +25,6 @@ import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.controller.ArmFeedforward;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RunCommand;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj.DutyCycleEncoder;
 import edu.wpi.first.math.controller.ProfiledPIDController;
@@ -37,7 +35,6 @@ import edu.wpi.first.wpilibj.Timer;
 
 /* Local */
 import frc.robot.Constants.CanConstants;
-import frc.robot.Util.TunableNumber;
 import frc.robot.Constants.DIOConstants;
 import frc.robot.Subsystems.Arm.Setpoint.ArmState;
 import frc.robot.Util.TunableNumber;
@@ -53,13 +50,19 @@ public class ArmSubsystem extends SubsystemBase {
     private DutyCycleOut m_PercentOutput = new DutyCycleOut(0.0);
 
     private TrapezoidProfile.Constraints armConstraints;
+
+    private ProfiledPIDController m_controllerarmLeader;
+
     private ArmFeedforward feedforward = new ArmFeedforward(Constants.ArmConstants.kS, Constants.ArmConstants.kG, Constants.ArmConstants.kV, Constants.ArmConstants.kA); // double kS, double kG, double kV, double kA
+
+    private Setpoint m_setpoint;
+    private double armDegrees;
+    
     public ArmSubsystem() {
         
         // Config Duty Cycle Range for the encoders
         m_encoder.setDutyCycleRange(ArmConstants.DUTY_CYCLE_MIN, ArmConstants.DUTY_CYCLE_MAX);
 
-        
         // Default Motors
         var leadConfiguration = new TalonFXConfiguration();
         var followerConfiguration = new TalonFXConfiguration();
@@ -85,16 +88,14 @@ public class ArmSubsystem extends SubsystemBase {
 
         Timer.delay(1.5); //TODO: See if needs to be removed
         armConstraints = new TrapezoidProfile.Constraints(ArmConstants.ARM_CRUISE,
-
-        Timer.delay(1.5); //TODO: See if needs to be removed
+                                                          ArmConstants.ARM_ACCELERATION);
 
         m_controllerarmLeader = new ProfiledPIDController(0.02, 0, 0, armConstraints);
         m_armLeader.getConfigurator().apply(leadConfiguration);
-        m_controllerarmLeader = new ProfiledPIDController(0.02, 0, 0, armConstraints);
 
-        m_armFollower.setControl(new Follower(m_armLeader.getDeviceID(), false));
         m_armFollower.getConfigurator().apply(followerConfiguration);
         m_armFollower.setControl(new Follower(m_armLeader.getDeviceID(), false));
+
         reset();
     }
 
@@ -103,16 +104,14 @@ public class ArmSubsystem extends SubsystemBase {
         // This method will be called once per scheduler run
 
         if(getArmJointDegrees() <= 0.0 || getArmJointDegrees() >= 360.0) {
-        if(getArmJointDegrees() <= 0.0 || getArmJointDegrees() >= 360.0) {
             m_armLeader.stopMotor();
+        }
         SmartDashboard.putData("Arm Contoller",m_controllerarmLeader);
-        SmartDashboard.putData("Arm Contoller",m_controllerarmLeader);
+        //SmartDashboard.putBoolean("Upper at Setpoint", getUpperAtSetpoint());
         SmartDashboard.putBoolean("Arm Joint at Setpoint", getArmJointAtSetpoint());
         //SmartDashboard.putNumber("Upper Angle", getUpperJointDegrees());
         SmartDashboard.putNumber("Arm Joint Angle", getArmJointDegrees());
 
-
-        SmartDashboard.putNumber("Arm Encoder", getArmJointPos());
         SmartDashboard.putNumber("Arm Encoder", getArmJointPos());
 
         if (Constants.RobotConstants.kIsTuningMode) {
@@ -124,6 +123,7 @@ public class ArmSubsystem extends SubsystemBase {
 
     public void reset() {
         armDegrees = getArmJointDegrees();
+        m_controllerarmLeader.reset(getArmJointDegrees());
         m_setpoint = new Setpoint(armDegrees, ArmState.OTHER);
         
     }
@@ -143,6 +143,7 @@ public class ArmSubsystem extends SubsystemBase {
         double ff = feedforward.calculate(pidOutput, 0);
         if(Constants.RobotConstants.kIsTuningMode){
             SmartDashboard.putNumber("Arm PID output", pidOutput);
+        }
         System.out.println("Upper PID" + pidOutput);
         if(Math.abs(pidOutput) > 0.01 && Math.abs(pidOutput)<0.045){
             pidOutput = Math.copySign(0.045, pidOutput);
@@ -172,20 +173,21 @@ public class ArmSubsystem extends SubsystemBase {
     }
 
     public Command runArmMotorSpeedCommand(double speed) {
-    public Command runArmMotorSpeedCommand(double speed) {
         return new RunCommand(() -> this.setPercentOutputUpper(speed),this);
     }
 
 /*     public void neutralUpper() {
         m_armLeader.neutralOutput();
     } */
-/*     public void neutralUpper() {
-        m_armLeader.neutralOutput();
-    } */
 
     public double getArmJointPos() {
         return m_encoder.getAbsolutePosition();
+    }
+
+    public double getArmJointDegrees() {
         return dutyCycleToDegrees(getArmJointPos()) + ArmConstants.ARM_OFFSET;
+    }
+
     public double dutyCycleToDegrees(double dutyCyclePos) {
         return dutyCyclePos * 360;
     }
